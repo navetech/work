ee941f60f5b0330f4e1fdb91fd5882e4c40f1a06
Ladislau Nave <ladislau.nave@terra.com.br>
2021-02-08 20:11:32 -0200

    Stucture database for menu meals

--- pizza/project3/orders/admin.py
+++ pizza/project3/orders/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import Size
 from .models import Topping, SpecialPizza, PizzaType, PizzaFlavor, Pizza
+from .models import SubFlavor, Sub, ExtraFlavor, Extra
+from .models import PastaFlavor, Pasta
+from .models import SaladFlavor, Salad
+from .models import DinnerPlatterFlavor, DinnerPlatter
 
 # Register your models here.
 admin.site.register(Size)
@@ -10,3 +14,13 @@ admin.site.register(SpecialPizza)
 admin.site.register(PizzaType)
 admin.site.register(PizzaFlavor)
 admin.site.register(Pizza)
+admin.site.register(SubFlavor)
+admin.site.register(Sub)
+admin.site.register(ExtraFlavor)
+admin.site.register(Extra)
+admin.site.register(PastaFlavor)
+admin.site.register(Pasta)
+admin.site.register(SaladFlavor)
+admin.site.register(Salad)
+admin.site.register(DinnerPlatterFlavor)
+admin.site.register(DinnerPlatter)
--- pizza/project3/orders/models.py
+++ pizza/project3/orders/models.py
@@ -46,9 +46,91 @@ class PizzaFlavor(models.Model):
 
 class Pizza(models.Model):
     pizza_type = models.ForeignKey(PizzaType, on_delete=models.CASCADE, related_name="type_pizzas")
-    pizza_size = models.ForeignKey(Size, on_delete=models.CASCADE, related_name="size_pizzas")
     flavor = models.ForeignKey(PizzaFlavor, on_delete=models.CASCADE, related_name="flavor_pizzas")
+    pizza_size = models.ForeignKey(Size, on_delete=models.CASCADE, related_name="size_pizzas")
     price = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
-        return f"type: {self.pizza_type}, size: {self.pizza_size}, flavor: {self.flavor}, price: {self.price}"
+        return f"type: {self.pizza_type}, flavor: {self.flavor}, size: {self.pizza_size}, price: {self.price}"
+
+
+class SubFlavor(models.Model):
+    sort_number = models.FloatField(default=0)
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.sort_number} - {self.name}"
+        
+
+class Sub(models.Model):
+    flavor = models.ForeignKey(SubFlavor, on_delete=models.CASCADE, related_name="flavor_subs")
+    sub_size = models.ForeignKey(Size, on_delete=models.CASCADE, related_name="size_subs")
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"flavor: {self.flavor}, size: {self.sub_size}, price: {self.price}"
+
+
+class ExtraFlavor(models.Model):
+    sort_number = models.FloatField(default=0)
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.sort_number} - {self.name}"
+        
+
+class Extra(models.Model):
+    flavor = models.ForeignKey(ExtraFlavor, on_delete=models.CASCADE, related_name="flavor_extras")
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"flavor: {self.flavor}, price: {self.price}"
+
+
+class PastaFlavor(models.Model):
+    sort_number = models.FloatField(default=0)
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.sort_number} - {self.name}"
+        
+
+class Pasta(models.Model):
+    flavor = models.ForeignKey(PastaFlavor, on_delete=models.CASCADE, related_name="flavor_pastas")
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"flavor: {self.flavor}, price: {self.price}"
+
+
+class SaladFlavor(models.Model):
+    sort_number = models.FloatField(default=0)
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.sort_number} - {self.name}"
+        
+
+class Salad(models.Model):
+    flavor = models.ForeignKey(SaladFlavor, on_delete=models.CASCADE, related_name="flavor_salads")
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"flavor: {self.flavor}, price: {self.price}"
+
+
+class DinnerPlatterFlavor(models.Model):
+    sort_number = models.FloatField(default=0)
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.sort_number} - {self.name}"
+        
+
+class DinnerPlatter(models.Model):
+    flavor = models.ForeignKey(DinnerPlatterFlavor, on_delete=models.CASCADE, related_name="flavor_dinnerplatters")
+    dinnerplatter_size = models.ForeignKey(Size, on_delete=models.CASCADE, related_name="size_dinnerplatters")
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"flavor: {self.flavor}, size: {self.dinnerplatter_size}, price: {self.price}"
