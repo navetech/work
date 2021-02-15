b3db028f6b0a5e12f5e0d587485374718c1a93fc
Ladislau Nave <ladislau.nave@terra.com.br>
2021-02-15 19:49:42 -0200

    Start implementing login, logout, register, unregister

--- pizza/project3/orders/views.py
+++ pizza/project3/orders/views.py
@@ -1,8 +1,69 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from django.contrib.auth import authenticate, login, logout, get_user
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
 # Create your views here.
 def index(request):
     #return HttpResponse("Project 3: TODO")
 
-    return render(request, "orders/layout.html")
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    else:
+        context = {
+            "user": request.user
+        }
+        return render(request, "orders/user.html", context)
+
+
+def login_view(request):
+    if request.method == "GET":
+        return render(request, "orders/login.html", {"message": None})
+    
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "orders/login.html", {"message": "User not active."})
+    else:
+        return render(request, "orders/login.html", {"message": "Invalid credentials."})
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register_view(request):
+    if request.method == "GET":
+        return render(request, "orders/register.html", {"message": None})
+
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is None:
+        user = User.objects.create_user(username=username, password=password)
+        if user is None:
+            return render(request, "orders/register.html", {"message": "Invalid credentials."})
+
+    login(request, user)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def unregister_view(request):
+    if request.method == "GET":
+        return render(request, "orders/unregister.html")
+
+    user = get_user(request)
+    user.is_active = False
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
