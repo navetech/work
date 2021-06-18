019cc99852c448f1dc32dd3bd91d30200062555d
Ladislau Nave <ladislau.nave@terra.com.br>
2021-06-18 15:53:37 -0300

    Create basic.languages, basic001.texts django apps

---
+++ django-projects/basic001/texts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+from basic.languages.models import Iso_639_LanguageCode
+
+class Text(models.Model):
+    words = models.CharField(max_length=256, blank=True)
+    language = models.ForeignKey(Iso_639_LanguageCode, blank=True, null=True, on_delete=models.CASCADE,
+        related_name="language_texts")
+
+    basics = models.ManyToManyField('self', blank=True, related_name="basics_texts")
+
+    def __str__(self):
+        return f"{self.words}, {self.language}, {self.basics}"
