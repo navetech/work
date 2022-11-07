3cbfe871f95117ac9929d50bca6e48d42d278998
Ladislau Nave <ladislau.nave@terra.com.br>
2022-11-07 18:43:47 -0200

    Create quadrado magico project

---
+++ quadrado-magico/README.md
@@ -0,0 +1,71 @@
+# [CS50's Web Programming with Python and JavaScript](https://cs50.harvard.edu/web/2018/)<br><br>
+
+
+## [Project 3: Pizza](https://docs.cs50.net/ocw/web/projects/3/project3.html)<br><br>
+
+
+### **Overview**<br>
+
+This is a project of a web application for handling online orders for a restaurant.
+
+Users (customers) can register for the website and then log in, using their username and password. Once they log in, they can browse the restaurant's menus, add items to their cart, and submit their orders, using a credit card to make a purchase during checkout.
+
+Users can also select a language for the website, as well select the currency for the menus' and orders' prices.
+
+<br>
+
+### **Features**<br>
+
+This project has the following main features:
+
+- **Registration:** Users can register for the website, providing a username and a password.
+
+- **Login:** Users, once registered, can log in on the website with their username and password.
+
+- **Logout**: Logged in users can log out of the site.
+
+- **Unregistration:** Users can unregister from the website.
+
+- **Menu Update:** Using Django Admin resources, site administrators (restaurant owners) can add, update and remove items on the menus.
+
+- **Menus:** Users can see the various kinds of available menus in the restaurant. If there is only a kind of menu, then users see the page of that one.
+
+- **Menu:** Once specified a menu, users can see a list the menu items, with their corresponding prices. Selecting an item, add it to the user's order.
+
+- **Orders Items:** Users can see the details of an order item, and choose addings (like toppings or extras, for example) to add to the item, as well specify the number of each adding or even specify the number of that item in the order.
+
+- **Shopping Cart:** Users can see the order items added to their virtual "shopping cart". The contents of the cart is saved even if a user closes the window or logs out and logs back in again.
+
+- **Checkout:** Users can submit their orders for checkout to make the purchase using a credit card.
+
+- **Select Language:** Users can select a language for the website.
+
+- **Select Currency:** Users can select a currency for the menus and orders prices.
+
+<br>
+
+### **Tools**<br>
+
+Mainly, this project makes use of the following tools:
+
+- **[Python](https://www.python.org/downloads/):** (*version 3.6 or higher*). The list of the *Python packages* that need to be installed in order to run the web application, is added to the *requirements.txt* file.
+
+- **[Django](https://www.djangoproject.com/):** A high-level Python web framework.
+
+- **HTML5**
+
+- **CSS3**
+
+- **[Bootstrap 4](https://getbootstrap.com/docs/4.0/getting-started/introduction/):** Bootstrap is a popular front-end open source toolkit for building responsive sites.
+
+- **[Sass](https://sass-lang.com/):** Sass is a stylesheet language that is compiled to CSS.
+
+- **JavaScript**
+
+- **[stripe](https://stripe.com/):** An online payment processing for internet businesses
+
+- **[Currency Converter](https://pypi.org/project/CurrencyConverter/):** This is a currency converter that uses historical rates against a reference currency (Euro).
+
+- **[Pillow](https://pypi.org/project/Pillow/):** A Python Imaging Library which adds image processing capabilities to the Python interpreter.
+
+- **Object-oriented Programming (OOP)** 
---
+++ quadrado-magico/quadrado-magico.py
@@ -0,0 +1,224 @@
+import csv
+import sys
+
+
+def load_data(directory):
+    """
+    Load data from CSV files into memory.
+    """
+
+    # Load data
+    data_loaded = {}
+    data_loaded["precision"] = 0.1
+    data_loaded["inline_defined_values"] = []
+
+    try:
+        with open(f"{directory}/precision.csv", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                data_loaded["precision"] = row["precision"]
+    except:
+        pass
+
+    try:
+        with open(f"{directory}/inline-defined-values.csv", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                data_loaded["inline_defined_values"].append(row["inline-defined-values"])
+    except:
+        pass
+
+    return data_loaded        
+        
+
+def main():
+    # Get command line arguments
+    if len(sys.argv) > 2:
+        sys.exit("Usage: python quadrado-magico.py [directory]")
+    directory = sys.argv[1] if len(sys.argv) == 2 else ""
+
+    # Load data from files into memory
+    print("Loading data...")
+    loaded_data = load_data(directory)
+    print("Data loaded.")
+
+    print(loaded_data)
+
+
+    # Build non-define values
+
+    # Calculate number of estimated equations
+
+    # Get permutations of non-defined values
+
+    # Get permutations of inline defined values
+
+    # Build fixed equations
+
+    # For each permutation of non-defined values
+
+        # Build estimated equations
+
+        # IF there are not any inline defined values
+
+            # Solve equations
+
+        # Else
+
+            # For each permutation of inline defioned values
+
+                # for each line of the magic square
+
+                    # Build equations for the line
+
+                    # Solve equations
+
+    # Save solutions
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+import csv
+import sys
+
+from util import Node, StackFrontier, QueueFrontier
+
+# Maps names to a set of corresponding person_ids
+names = {}
+
+# Maps person_ids to a dictionary of: name, birth, movies (a set of movie_ids)
+people = {}
+
+# Maps movie_ids to a dictionary of: title, year, stars (a set of person_ids)
+movies = {}
+"""
+
+def load_data2(directory):
+    """
+    Load data from CSV files into memory.
+    """
+    # Load people
+    with open(f"{directory}/people.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            people[row["id"]] = {
+                "name": row["name"],
+                "birth": row["birth"],
+                "movies": set()
+            }
+            if row["name"].lower() not in names:
+                names[row["name"].lower()] = {row["id"]}
+            else:
+                names[row["name"].lower()].add(row["id"])
+
+    # Load movies
+    with open(f"{directory}/movies.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            movies[row["id"]] = {
+                "title": row["title"],
+                "year": row["year"],
+                "stars": set()
+            }
+
+    # Load stars
+    with open(f"{directory}/stars.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            try:
+                people[row["person_id"]]["movies"].add(row["movie_id"])
+                movies[row["movie_id"]]["stars"].add(row["person_id"])
+            except KeyError:
+                pass
+
+
+def main2():
+    if len(sys.argv) > 2:
+        sys.exit("Usage: python degrees.py [directory]")
+    directory = sys.argv[1] if len(sys.argv) == 2 else "large"
+
+    # Load data from files into memory
+    print("Loading data...")
+    load_data(directory)
+    print("Data loaded.")
+
+    source = person_id_for_name(input("Name: "))
+    if source is None:
+        sys.exit("Person not found.")
+    target = person_id_for_name(input("Name: "))
+    if target is None:
+        sys.exit("Person not found.")
+
+    path = shortest_path(source, target)
+
+    if path is None:
+        print("Not connected.")
+    else:
+        degrees = len(path)
+        print(f"{degrees} degrees of separation.")
+        path = [(None, source)] + path
+        for i in range(degrees):
+            person1 = people[path[i][1]]["name"]
+            person2 = people[path[i + 1][1]]["name"]
+            movie = movies[path[i + 1][0]]["title"]
+            print(f"{i + 1}: {person1} and {person2} starred in {movie}")
+
+
+def shortest_path(source, target):
+    """
+    Returns the shortest list of (movie_id, person_id) pairs
+    that connect the source to the target.
+
+    If no possible path, returns None.
+    """
+
+    # TODO
+    raise NotImplementedError
+
+
+def person_id_for_name(name):
+    """
+    Returns the IMDB id for a person's name,
+    resolving ambiguities as needed.
+    """
+    person_ids = list(names.get(name.lower(), set()))
+    if len(person_ids) == 0:
+        return None
+    elif len(person_ids) > 1:
+        print(f"Which '{name}'?")
+        for person_id in person_ids:
+            person = people[person_id]
+            name = person["name"]
+            birth = person["birth"]
+            print(f"ID: {person_id}, Name: {name}, Birth: {birth}")
+        try:
+            person_id = input("Intended Person ID: ")
+            if person_id in person_ids:
+                return person_id
+        except ValueError:
+            pass
+        return None
+    else:
+        return person_ids[0]
+
+
+def neighbors_for_person(person_id):
+    """
+    Returns (movie_id, person_id) pairs for people
+    who starred with a given person.
+    """
+    movie_ids = people[person_id]["movies"]
+    neighbors = set()
+    for movie_id in movie_ids:
+        for person_id in movies[movie_id]["stars"]:
+            neighbors.add((movie_id, person_id))
+    return neighbors
+
+"""
+if __name__ == "__main__":
+    main()
+
+"""
